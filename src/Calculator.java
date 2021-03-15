bf9679337844e5dcb69764b58e121397bedaf2bc
Sergey Milyukov <serrguk@gmail.com>
2021-03-15 12:16:19 +0700

    comments added

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Calculator {
+    // В полях класса объявляем все компоненты калькулятора
     JPanel windowsContent;
     JTextField displayField;
     JButton button0;
@@ -19,14 +20,24 @@ public class Calculator {
 
     JPanel pl;
 
+    // В конструкторе инициализируем все поля соответствующими объектами
+    // и добавляем на фрейм (окно) с помощью комбинации
+    //BorderLayout и GridLayout
     Calculator() {
+        // Инициализируем панель...
          windowsContent = new JPanel();
+
+         // ...и создаём для неё схему
         BorderLayout bl = new BorderLayout();
         windowsContent.setLayout(bl);
 
+        //Создаём и отображаем поле
+        //И помещаем его в Северную область окна
         displayField = new JTextField(30);
         windowsContent.add("North", displayField);
 
+        //Создаём кнопки, используя конструктор класса JButton,
+        // который принимает текст кнопки в качестве аргумента
         button0 = new JButton("0");
         button1 = new JButton("1");
         button2 = new JButton("2");
@@ -40,10 +51,14 @@ public class Calculator {
         buttonPoint = new JButton(".");
         buttonEquals = new JButton("=");
 
+        //Создаём панель с GridLayout (панель сеткой) размером 4х3,
+        // которая содержит 12 кнопок -
+        //10 кнопок с цифрами и кнопки с точкой и знаком равно.
         pl = new JPanel();
         GridLayout gl = new GridLayout(4, 3);
         pl.setLayout(gl);
 
+        //Добавляем кнопки на панель pl.
         pl.add(button0);
         pl.add(button1);
         pl.add(button2);
@@ -57,13 +72,17 @@ public class Calculator {
         pl.add(buttonPoint);
         pl.add(buttonEquals);
 
+        //Помещаем панель pl в центральную область окна.
         windowsContent.add("Center", pl);
 
+        // Создаём фрейм и задаём его основную панель.
         JFrame frame = new JFrame("It's fucking plus-minus");
         frame.setContentPane(windowsContent);
 
+        //Делаем размер окна достаточным, чтобы вместить все элементы.
         frame.pack();
 
+        //Делаем окно видимым.
         frame.setVisible(true);
     }
 }
