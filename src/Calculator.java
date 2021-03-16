a4494f38ddcebfa533b0602925db59981941ae5c
Milyukov Sergey <serrguk@gmail.om>
2021-03-17 00:33:11 +0700

    comments added

--- src/Calculator.java
+++ src/Calculator.java
@@ -31,6 +31,7 @@ public class Calculator {
         //Создаём и отображаем поле
         //И помещаем его в Северную область окна
         displayField = new JTextField(30);
+
         windowsContent.add("North", displayField);
 
         //Создаём кнопки, используя конструктор класса JButton,
@@ -51,11 +52,11 @@ public class Calculator {
         // которая содержит 12 кнопок -
         //10 кнопок с цифрами и кнопки с точкой и знаком равно.
         p1 = new JPanel();
-        GridLayout gl = new GridLayout(4, 3);
+        GridLayout gl = new GridLayout(4, 3, 5, 5);
         p1.setLayout(gl);
 
         p2 = new JPanel();
-        GridLayout g2 = new GridLayout(4, 1);
+        GridLayout g2 = new GridLayout(4, 1, 5, 5);
         p2.setLayout(g2);
 
         //Добавляем кнопки на панель pl.
@@ -71,11 +72,11 @@ public class Calculator {
         p2.add(buttonMultiply);
         p2.add(buttonDivided);
 
-        //Помещаем панель pl в центральную область окна.
+        //Помещаем панель p1 в центральную область окна, а p2 - в восточную.
         windowsContent.add("Center", p1);
         windowsContent.add("East", p2);
 
-        // Создаём фрейм и задаём его основную панель.
+        // Создаём фрейм(в параметрах пишем название) и задаём его основную панель.
         JFrame frame = new JFrame("It's fucking plus-minus");
         frame.setContentPane(windowsContent);
 
@@ -84,5 +85,25 @@ public class Calculator {
 
         //Делаем окно видимым.
         frame.setVisible(true);
+
+        CalculatorEngine calcEngine = new CalculatorEngine(this);
+
+        numButtons[0].addActionListener(calcEngine);
+        numButtons[1].addActionListener(calcEngine);
+        numButtons[2].addActionListener(calcEngine);
+        numButtons[3].addActionListener(calcEngine);
+        numButtons[4].addActionListener(calcEngine);
+        numButtons[5].addActionListener(calcEngine);
+        numButtons[6].addActionListener(calcEngine);
+        numButtons[7].addActionListener(calcEngine);
+        numButtons[8].addActionListener(calcEngine);
+        numButtons[9].addActionListener(calcEngine);
+
+        buttonPoint.addActionListener(calcEngine);
+        buttonEquals.addActionListener(calcEngine);
+        buttonPlus.addActionListener(calcEngine);
+        buttonMinus.addActionListener(calcEngine);
+        buttonMultiply.addActionListener(calcEngine);
+        buttonDivided.addActionListener(calcEngine);
     }
 }
---
+++ src/CalculatorEngine.java
@@ -0,0 +1,79 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CalculatorEngine implements ActionListener {
+    Calculator parent; //ссылка на окно калькулятора
+    char selectedAction = ' '; //переменная для арифметических действий
+    double currentResult = 0; //переменная, где будет храниться результат действий
+
+
+    //Конструктор сохраняет ссылку на окно калькулятора
+    //в переменной экземпляра класса
+    CalculatorEngine(Calculator parent) {
+        this.parent = parent;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //Получаем источник действия(нажатия на кнопку)
+        JButton clickedButton = (JButton) e.getSource();
+        String dispFieldText = parent.displayField.getText();
+
+        double displayValue = 0;
+
+
+        //Получаем значение из дисплея калькулятора, если он не пустой
+        if (!"".equals(dispFieldText)) {
+            displayValue = Double.parseDouble(dispFieldText);
+        }
+        Object src = e.getSource();
+        /*
+        Для каждой кнопки арифметического действия -
+        запомнить его, сохранить текущее число в переменной
+        currentResult и очистить дисплей для ввода нового числа
+         */
+
+        if (src == parent.buttonPlus) {
+            selectedAction = '+';
+            currentResult = displayValue;
+            parent.displayField.setText("");
+        } else if (src == parent.buttonMinus) {
+            selectedAction = '-';
+            currentResult = displayValue;
+            parent.displayField.setText("");
+        } else if (src == parent.buttonDivided) {
+            selectedAction = '/';
+            currentResult = displayValue;
+            parent.displayField.setText("");
+        } else if (src == parent.buttonMultiply) {
+            selectedAction = '*';
+            currentResult = displayValue;
+            parent.displayField.setText("");
+        } else if (src == parent.buttonEquals) {
+            /*
+            Если нажата кнопка "=" - совершить арифметическое действие,
+            в зависимости от selectedAction, обновить переменную
+            currentResult и вернуть результат на дисплей
+             */
+            if (selectedAction == '+') {
+                currentResult += displayValue;
+                parent.displayField.setText("" + currentResult);
+            } else if (selectedAction == '-') {
+                currentResult -= displayValue;
+                parent.displayField.setText("" + currentResult);
+            } else if (selectedAction == '/') {
+                currentResult /= displayValue;
+                parent.displayField.setText("" + currentResult);
+            } else if (selectedAction == '*') {
+                currentResult *= displayValue;
+                parent.displayField.setText("" + currentResult);
+            } else {
+                //Для всех цифровых кнопок - присоединить надпись
+                //на кнопке к надписи на дисплее
+                String clickedButtonLabel = clickedButton.getText();
+                parent.displayField.setText(dispFieldText + clickedButtonLabel);
+            }
+        }
+    }
+}
