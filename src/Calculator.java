62433da6a2ef0e777c53362ef9a24d2f45e1cd2b
Sergey Milyukov <serrguk@gmail.com>
2021-03-16 14:11:24 +0700

    change class Calculator, four buttons added

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,29 +5,26 @@ public class Calculator {
     // В полях класса объявляем все компоненты калькулятора
     JPanel windowsContent;
     JTextField displayField;
-    JButton button0;
-    JButton button1;
-    JButton button2;
-    JButton button3;
-    JButton button4;
-    JButton button5;
-    JButton button6;
-    JButton button7;
-    JButton button8;
-    JButton button9;
+    JButton[] numButtons = new JButton[10];
+
+    JButton buttonPlus;
+    JButton buttonMinus;
+    JButton buttonDivided;
+    JButton buttonMultiply;
     JButton buttonPoint;
     JButton buttonEquals;
 
-    JPanel pl;
+    JPanel p1;
+    JPanel p2;
 
     // В конструкторе инициализируем все поля соответствующими объектами
     // и добавляем на фрейм (окно) с помощью комбинации
     //BorderLayout и GridLayout
     Calculator() {
         // Инициализируем панель...
-         windowsContent = new JPanel();
+        windowsContent = new JPanel();
 
-         // ...и создаём для неё схему
+        // ...и создаём для неё схему
         BorderLayout bl = new BorderLayout();
         windowsContent.setLayout(bl);
 
@@ -38,42 +35,45 @@ public class Calculator {
 
         //Создаём кнопки, используя конструктор класса JButton,
         // который принимает текст кнопки в качестве аргумента
-        button0 = new JButton("0");
-        button1 = new JButton("1");
-        button2 = new JButton("2");
-        button3 = new JButton("3");
-        button4 = new JButton("4");
-        button5 = new JButton("5");
-        button6 = new JButton("6");
-        button7 = new JButton("7");
-        button8 = new JButton("8");
-        button9 = new JButton("9");
+        for (int i = 0; i < numButtons.length; i++) {
+            String s = String.valueOf(i);
+            numButtons[i] = new JButton(s);
+        }
+
+        buttonPlus = new JButton("+");
+        buttonMinus = new JButton("-");
+        buttonDivided = new JButton("/");
+        buttonMultiply = new JButton("*");
         buttonPoint = new JButton(".");
         buttonEquals = new JButton("=");
 
         //Создаём панель с GridLayout (панель сеткой) размером 4х3,
         // которая содержит 12 кнопок -
         //10 кнопок с цифрами и кнопки с точкой и знаком равно.
-        pl = new JPanel();
+        p1 = new JPanel();
         GridLayout gl = new GridLayout(4, 3);
-        pl.setLayout(gl);
+        p1.setLayout(gl);
+
+        p2 = new JPanel();
+        GridLayout g2 = new GridLayout(4, 1);
+        p2.setLayout(g2);
 
         //Добавляем кнопки на панель pl.
-        pl.add(button0);
-        pl.add(button1);
-        pl.add(button2);
-        pl.add(button3);
-        pl.add(button4);
-        pl.add(button5);
-        pl.add(button6);
-        pl.add(button7);
-        pl.add(button8);
-        pl.add(button9);
-        pl.add(buttonPoint);
-        pl.add(buttonEquals);
+
+        for (JButton numButton : numButtons) {
+            p1.add(numButton);
+        }
+        p1.add(buttonPoint);
+        p1.add(buttonEquals);
+
+        p2.add(buttonPlus);
+        p2.add(buttonMinus);
+        p2.add(buttonMultiply);
+        p2.add(buttonDivided);
 
         //Помещаем панель pl в центральную область окна.
-        windowsContent.add("Center", pl);
+        windowsContent.add("Center", p1);
+        windowsContent.add("East", p2);
 
         // Создаём фрейм и задаём его основную панель.
         JFrame frame = new JFrame("It's fucking plus-minus");
